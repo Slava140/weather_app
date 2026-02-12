7e5f68da95abaf3826021a27595d596d1f82f42b
kenss <slava20031112@gmail.com>
2026-02-12 21:14:11 +0500

    Небольшие правки

--- lib/models/weather.dart
+++ lib/models/weather.dart
@@ -41,31 +41,10 @@ class Current {
   final num tempF;
   final num isDay;
   final Condition condition;
-  final num windMph;
   final num windKph;
-  final num windDegree;
-  final String windDir;
   final num pressureMb;
-  final num pressureIn;
-  final num precipMm;
-  final num precipIn;
   final num humidity;
-  final num cloud;
-  final num feelslikeC;
-  final num feelslikeF;
-  final num windchillC;
-  final num windchillF;
-  final num heatindexC;
-  final num heatindexF;
   final num visKm;
-  final num visMiles;
-  final num uv;
-  final num gustMph;
-  final num gustKph;
-  final num shortRad;
-  final num diffRad;
-  final num dni;
-  final num gti;
 
   const Current({
     required this.lastUpdatedEpoch,
@@ -74,31 +53,10 @@ class Current {
     required this.tempF,
     required this.isDay,
     required this.condition,
-    required this.windMph,
     required this.windKph,
-    required this.windDegree,
-    required this.windDir,
     required this.pressureMb,
-    required this.pressureIn,
-    required this.precipMm,
-    required this.precipIn,
     required this.humidity,
-    required this.cloud,
-    required this.feelslikeC,
-    required this.feelslikeF,
-    required this.windchillC,
-    required this.windchillF,
-    required this.heatindexC,
-    required this.heatindexF,
     required this.visKm,
-    required this.visMiles,
-    required this.uv,
-    required this.gustMph,
-    required this.gustKph,
-    required this.shortRad,
-    required this.diffRad,
-    required this.dni,
-    required this.gti,
   });
 
   factory Current.fromJson(Map<String, dynamic> json) {
@@ -109,31 +67,10 @@ class Current {
         tempF: json['temp_f'],
         isDay: json['is_day'],
         condition: Condition.fromJson(json['condition']),
-        windMph: json['wind_mph'],
         windKph: json['wind_kph'],
-        windDegree: json['wind_degree'],
-        windDir: json['wind_dir'],
         pressureMb: json['pressure_mb'],
-        pressureIn: json['pressure_in'],
-        precipMm: json['precip_mm'],
-        precipIn: json['precip_in'],
         humidity: json['humidity'],
-        cloud: json['cloud'],
-        feelslikeC: json['feelslike_c'],
-        feelslikeF: json['feelslike_f'],
-        windchillC: json['windchill_c'],
-        windchillF: json['windchill_f'],
-        heatindexC: json['heatindex_c'],
-        heatindexF: json['heatindex_f'],
         visKm: json['vis_km'],
-        visMiles: json['vis_miles'],
-        uv: json['uv'],
-        gustMph: json['gust_mph'],
-        gustKph: json['gust_kph'],
-        shortRad: json['short_rad'],
-        diffRad: json['diff_rad'],
-        dni: json['dni'],
-        gti: json['gti'],
     );
   }
 }
